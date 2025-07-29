d8a2de45723a412642b83c3f1b554c017ffffa54
prof-mileniorocha <127246074+prof-mileniorocha@users.noreply.github.com>
2025-07-29 18:27:18 -0300

    Corrigindo código

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,3 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
 });
-
-/// src/index.css
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-
-/// src/main.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Dashboard from './Dashboard';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Dashboard />
-  </React.StrictMode>
-);
